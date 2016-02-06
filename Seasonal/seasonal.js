1eb904e3106ad10e2a4670016e7cf990202b93f9
Mark Strendin <mark.strendin@gmail.com>
2016-02-06 16:19:08 -0600

    Added the rest of the major holidays to decorate for, and added a few more blank ones to play around with in the future.

--- Seasonal/seasonal.js
+++ Seasonal/seasonal.js
@@ -27,8 +27,96 @@ console.log("Current day is: " + currentDay);
 
 // Valentines
 if (currentMonth == 2) {
-    if ((currentDay >= 1) && (currentDay <= 14)) {
+    if ((currentDay >= 7) && (currentDay <= 14)) {
         console.log("Loading valentines day stylesheets");
         LoadCSSFile("../Seasonal/Valentines/seasonal.css");
     }
 }
+
+// Easter (2016): March 27
+// Easter is always the first sunday after the first full moon on or after the vernal equinox
+// Easter (2017): April 16
+// Easter (2018): April 1
+// Easter (2019): April 21
+// Easter (2020): April 12
+// Easter (2021): April 4
+if (
+    ((currentYear == 2016) && (currentMonth == 3) && (currentDay >= 22) && (currentDay <= 27)) ||
+    ((currentYear == 2017) && (currentMonth == 4) && (currentDay >= 11) && (currentDay <= 16)) ||
+    ((currentYear == 2018) && (currentMonth == 3) && (currentDay >= 26) && (currentDay <= 31)) ||
+    ((currentYear == 2018) && (currentMonth == 4) && (currentDay >= 1) && (currentDay <= 1)) ||
+    ((currentYear == 2019) && (currentMonth == 4) && (currentDay >= 16) && (currentDay <= 21)) ||
+    ((currentYear == 2020) && (currentMonth == 4) && (currentDay >= 6) && (currentDay <= 12)) ||
+    ((currentYear == 2021) && (currentMonth == 3) && (currentDay >= 26) && (currentDay <= 31)) ||
+    ((currentYear == 2021) && (currentMonth == 4) && (currentDay >= 1) && (currentDay <= 4))
+    ) {
+    console.log("Loading Easter stylesheets");
+    LoadCSSFile("../Seasonal/Easter/seasonal.css");
+}
+
+// Halloween
+if (currentMonth == 10) {
+    if ((currentDay >= 26) && (currentDay <= 31)) {
+        console.log("Loading Halloween stylesheets");
+        LoadCSSFile("../Seasonal/Halloween/seasonal.css");
+    }
+}
+
+// Remembrance Day
+if (currentMonth == 11) {
+    if ((currentDay >= 10) && (currentDay <= 11)) {
+        console.log("Loading Remembrance day stylesheets");
+        LoadCSSFile("../Seasonal/Nov11/seasonal.css");
+    }
+}
+
+// Spring/Vernal equinox
+if (currentMonth == 3) {
+    if ((currentDay >= 20) && (currentDay <= 20)) {
+        console.log("Loading vernal equinox stylesheets");
+        LoadCSSFile("../Seasonal/SpringEquinox/seasonal.css");
+    }
+}
+
+// Summer Solstice
+if (currentMonth == 6) {
+    if ((currentDay >= 21) && (currentDay <= 21)) {
+        console.log("Loading summer solstice stylesheets");
+        LoadCSSFile("../Seasonal/SummerSolstice/seasonal.css");
+    }
+}
+
+// Autumnal Equinox
+if (currentMonth == 9) {
+    if ((currentDay >= 22) && (currentDay <= 23)) {
+        console.log("Loading autumnal equinox stylesheets");
+        LoadCSSFile("../Seasonal/AutumnalEquinox/seasonal.css");
+    }
+}
+
+// Winter solstice
+if (currentMonth == 12) {
+    if ((currentDay >= 21) && (currentDay <= 21)) {
+        console.log("Loading winter solstice stylesheets");
+        LoadCSSFile("../Seasonal/WinterSolstice/seasonal.css");
+    }
+}
+
+// St Patrick's Day
+if (currentMonth == 3) {
+    if ((currentDay >= 12) && (currentDay <= 17)) {
+        console.log("Loading st patrick's day stylesheets");
+        LoadCSSFile("../Seasonal/StPatricks/seasonal.css");
+    }
+}
+
+// XMas
+if (currentMonth == 12) {
+    if ((currentDay >= 12) && (currentDay <= 25)) {
+        console.log("Loading Xmas stylesheets");
+        LoadCSSFile("../Seasonal/Xmas/seasonal.css");
+    }
+}
+
+
+
