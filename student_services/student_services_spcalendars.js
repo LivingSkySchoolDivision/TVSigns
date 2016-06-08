f55a9e120c6f31f98f46c434f54635405112b47f
Mark Strendin <mark.strendin@lskysd.ca>
2016-06-08 11:55:01 -0600

    Added meeting room and vehicle information to student services dashboard Changed the green color used in the vehicle list to a brighter green

--- student_services/student_services_spcalendars.js
+++ student_services/student_services_spcalendars.js
@@ -27,3 +27,28 @@ function updateVehicleStatus(divid, JSONURL) {
         });
     });
 }
+
+function updateCurrentRoomStatus(divid, roomJSONURL) {
+    var CurrentEvents = new Array();
+
+    !$('#' + divid).html("Loading..");
+
+    // Read JSON
+    // Find current events
+    $.getJSON(roomJSONURL, function(data) {
+
+        !$('#' + divid).html("Empty");
+
+        $.each(data.rightnow, function(j, ce) {
+
+            !$('#' + divid).html(ce.title);
+
+            // This doesn't work on the TV for some reason
+            if (!ce.title.startsWith("Deleted"))
+            {
+                //!$('#' + divid).html("nondeleted events found");
+                //CurrentEvents[CurrentEvents.length] = new NewCalendarItem(ce.id, ce.title, ce.startdatefriendly, ce.starttime, ce.enddate, ce.endtime, ce.description);}
+            }
+        });
+    });
+}
