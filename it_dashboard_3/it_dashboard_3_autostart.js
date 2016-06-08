5bc69ffecba1524493a9678bffb9850a1e1ffb26
Mark Strendin <mark.strendin@lskysd.ca>
2016-06-08 09:46:45 -0600

    Added division vehicles to the dashboard 3 Changed how meeting rooms are handled, because the Javascript on the TV's web browser doesn't support some string functions, causing the code to behave differently.

--- it_dashboard_3/it_dashboard_3_autostart.js
+++ it_dashboard_3/it_dashboard_3_autostart.js
@@ -6,6 +6,11 @@ $(document).ready(function(){
     updateCurrentRoomStatus('room_meeting', 'https://dashboard.lskysd.ca/LSKYDashboardDataCollector/Sharepoint2013/Calendar.aspx?url=https://portal.lskysd.ca/officecalendars&guid={5e88108d-3c75-4186-8a44-51d64a75d78d}');
     updateCurrentRoomStatus('room_ss', 'https://dashboard.lskysd.ca/LSKYDashboardDataCollector/Sharepoint2013/Calendar.aspx?url=https://portal.lskysd.ca/officecalendars&guid={0c6fab6f-8e0f-47db-9d3b-9cd2896be6d8}');
     updateCurrentRoomStatus('room_smallmeeting', 'https://dashboard.lskysd.ca/LSKYDashboardDataCollector/Sharepoint2013/Calendar.aspx?url=https://portal.lskysd.ca/officecalendars&guid={4F8BEC25-C9C5-4F4B-816E-3A6B68DEF37D}');
+
+    updateVehicleStatus('veh_gj', 'https://dashboard.lskysd.ca/LSKYDashboardDataCollector/Sharepoint2013/Calendar.aspx?url=https://portal.lskysd.ca/officecalendars&guid={892B6A8F-6E53-4E8A-ACA4-0EAD3EED99B3}');
+    updateVehicleStatus('veh_bj', 'https://dashboard.lskysd.ca/LSKYDashboardDataCollector/Sharepoint2013/Calendar.aspx?url=https://portal.lskysd.ca/officecalendars&guid={F46DDBAD-6825-4884-B13B-DE51423CBAF3}');
+    updateVehicleStatus('veh_sj', 'https://dashboard.lskysd.ca/LSKYDashboardDataCollector/Sharepoint2013/Calendar.aspx?url=https://portal.lskysd.ca/officecalendars&guid={ED1EE0C3-6D01-4032-ACDE-3685DEA2DDCE}');
+    updateVehicleStatus('veh_jeep', 'https://dashboard.lskysd.ca/LSKYDashboardDataCollector/Sharepoint2013/Calendar.aspx?url=https://portal.lskysd.ca/officecalendars&guid={953A6778-9A3E-4203-B3D6-A5C312BD01F4}');
 });
 
 /*
@@ -20,5 +25,8 @@ $(document).ready(function(){
 
 
 setInterval(function() {
-
+    updateCurrentRoomStatus('room_board', 'https://dashboard.lskysd.ca/LSKYDashboardDataCollector/Sharepoint2013/Calendar.aspx?url=https://portal.lskysd.ca/officecalendars&guid={d492d463-8204-460c-99a2-81f7646fb65e}');
+    updateCurrentRoomStatus('room_meeting', 'https://dashboard.lskysd.ca/LSKYDashboardDataCollector/Sharepoint2013/Calendar.aspx?url=https://portal.lskysd.ca/officecalendars&guid={5e88108d-3c75-4186-8a44-51d64a75d78d}');
+    updateCurrentRoomStatus('room_ss', 'https://dashboard.lskysd.ca/LSKYDashboardDataCollector/Sharepoint2013/Calendar.aspx?url=https://portal.lskysd.ca/officecalendars&guid={0c6fab6f-8e0f-47db-9d3b-9cd2896be6d8}');
+    updateCurrentRoomStatus('room_smallmeeting', 'https://dashboard.lskysd.ca/LSKYDashboardDataCollector/Sharepoint2013/Calendar.aspx?url=https://portal.lskysd.ca/officecalendars&guid={4F8BEC25-C9C5-4F4B-816E-3A6B68DEF37D}');
 }, 600000);
--- it_dashboard_3/it_dashboard_3_vehicles.js
+++ it_dashboard_3/it_dashboard_3_vehicles.js
@@ -1,21 +1,22 @@
 /**
- * Created by Mark on 6/7/2016.
+ * Created by mark.strendin on 2016-06-08.
  */
 
-function updateCurrentRoomStatus(divid, roomJSONURL) {
+function updateVehicleStatus(divid, JSONURL) {
     var CurrentEvents = new Array();
 
     !$('#' + divid).html("Loading..");
 
     // Read JSON
     // Find current events
-    $.getJSON(roomJSONURL, function(data) {
+    $.getJSON(JSONURL, function(data) {
 
-        !$('#' + divid).html("Empty");
+        !$('#' + divid).html("<span style='color: green;'>Available</span>");
 
         $.each(data.rightnow, function(j, ce) {
 
-            !$('#' + divid).html(ce.title);
+            //!$('#' + divid).html(ce.title);
+            !$('#' + divid).html("<span style='color: rgba(255,255,255,0.1);'>" + ce.title + "</span>");
 
             // This doesn't work on the TV for some reason
             if (!ce.title.startsWith("Deleted"))
