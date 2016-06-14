0c5d42a2ccc894086cf350dcad3a54654c3250ce
Mark Strendin <mark.strendin@lskysd.ca>
2016-06-14 17:34:47 -0600

    Working on adding a photo gallery to the student services page. It's basic functionality is working, but it's still a work in progress. I'm committing to save and move computers, and also because theres a thunderstorm here.

---
+++ student_services/student_services_photo_gallery.js
@@ -0,0 +1,35 @@
+/**
+ * Created by mark.strendin on 2016-06-14.
+ */
+
+var imageArray =  new Array();
+var imageArrayIndex = 0;
+
+function UpdatePhotoList(JSONURL, div) {
+    $.getJSON(JSONURL, function(data) {
+        // Figure out the size of the array
+        var arraySize = data.TotalPhotos;
+        imageArray = new Array(arraySize);
+
+        var x = 0;
+        $.each(data.Photos, function(j, photoPath) {
+            imageArray[x] = photoPath;
+            x++;
+        });
+    });
+}
+
+
+var imageIndex = 0;
+function NextGalleryImage() {
+    // Cycles to the next image
+
+    !$("#photo_tile").css("background-image", "url(" + imageArray[imageIndex] + ")");
+
+    //alert(imageArray[imageIndex]);
+
+    imageIndex++;
+    if (imageIndex >=  imageArray.length) {
+        imageIndex = 0;
+    }
+}
