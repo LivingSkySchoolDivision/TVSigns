ec761406602e26f12c422107461836751719544c
Mark Strendin <mark.strendin@lskysd.ca>
2016-07-27 10:13:15 -0600

    Added mostly empty TV dashboard for transportation, to be filled in later

---
+++ transportation/transportation_autostart.js
@@ -0,0 +1,37 @@
+/* ******************************************** */
+/* * Document onLoad stuff                    * */
+/* ******************************************** */
+$(document).ready(function(){
+    updateWeather();
+    updateDateAndTime();
+});
+
+
+/* ******************************************** */
+/* * Interval stuff                           * */
+/* ******************************************** */
+
+/*
+ 1000     1 second
+ 10000     10 seconds
+ 60000     1 minute
+ 300000     5 minutes
+ 600000     10 minutes
+ 1800000     30 mins
+ 3600000     1 hour
+ */
+
+/* Update weather */
+setInterval(function() {
+    updateWeather();
+}, 60000);
+
+/* Update the date and time displays */
+setInterval(function() {
+    updateDateAndTime();
+}, 500);
+
+/* Refresh the page every hour ish */
+setInterval(function() {
+    location.reload();
+}, 4500000);
