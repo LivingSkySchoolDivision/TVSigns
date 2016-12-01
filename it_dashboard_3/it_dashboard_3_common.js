54803b4f08b3f598faa92a75ad122fd18204fa76
Mark Strendin <mark.strendin@lskysd.ca>
2016-12-01 12:28:23 -0600

    Added guest account counter to dashboard 3

--- it_dashboard_3/it_dashboard_3_common.js
+++ it_dashboard_3/it_dashboard_3_common.js
@@ -8,3 +8,15 @@ function DaysUntil (year, month, day) {
     var firstDate = new Date();
     return Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)));
 }
+
+function UpdateGuestAccounts() {
+    var JSONPath = dashboardDataCollectorRoot + "/Proxy/XML.aspx?url=https://guests.lskysd.ca/JSON/";
+
+    $.getJSON(JSONPath, function(data) {
+
+        var activeGuestAccounts = data.TotalActive;
+
+        $('#guest_accounts_active').html(activeGuestAccounts);
+
+    });
+}
